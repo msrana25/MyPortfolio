3e6eb175180237ed9f4b920bcb55443f8c8ba1c9
msrana25 <mnprtrana61@gmail.com>
2024-05-21 16:22:56 -0400

    Adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,32 +4,44 @@ import Header from '@/components/headers'
 import ActiveSectionContextProvider from '@/context/active-section-context'
 import { Toaster } from 'react-hot-toast'
 import Footer from '@/components/footer'
+import Head from 'next/head'
+import Script from 'next/script'
+import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Manpreet Singh | Personal Portfolio',
-  description: 'Manpreet is a Full Stack Software Engineer with a Master’s degree and over 3.7 years of work experience in the Tech industry .',
+  description: 'Manpreet is a Full Stack Software Engineer with a Master’s degree and over 3.7 years of work experience in the Tech industry.',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className='!scroll-smooth'>
-      <body className={`${inter.className}
-       bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}>
-        <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rm]'></div>
-        <div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rm] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36`}>
+        <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
+        <div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
         <ActiveSectionContextProvider>
           <Header />
-           {children}
-          <Footer/>
-           <Toaster />
+          {children}
+          <Footer />
+          <Toaster />
         </ActiveSectionContextProvider>
-        </body>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-8KX5TQVHWG"></Script>
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8KX5TQVHWG');
+          `}
+        </Script>
+      </body>
     </html>
   )
 }
