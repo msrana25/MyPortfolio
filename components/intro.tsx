96f54a5baab4f56b142f820eb2cedbe5ed2154e0
msrana25 <mnprtrana61@gmail.com>
2024-08-03 14:17:44 -0400

    Layout changes

--- components/intro.tsx
+++ components/intro.tsx
@@ -52,7 +52,7 @@ export default function Intro() {
      animate = {{ opacity: 1, y:0 }}>
         <span className="font-bold">Hello, I&apos;m Manpreet Singh.</span> I&apos;m a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold"> 3 years</span> of work experience. I enjoy
+        <span className="font-bold"> 4 years</span> of work experience. I enjoy
         building <span className="italic">websites & applications</span>. My focus is{" "}
         <span className="underline">React, Next.js, Node.js and DevOps</span>.
      </motion.h1>
